eff93e512e371847f3d1904dcf3a0339d1b0a283
Carlos <carlosthe19916@gmail.com>
2015-10-23 18:59:44 -0500

    titulares

--- modules/socio/client/controllers/cuentaPersonal/editar.titulares.client.controller.js
+++ modules/socio/client/controllers/cuentaPersonal/editar.titulares.client.controller.js
@@ -2,15 +2,31 @@
 
 /* jshint -W098 */
 angular.module('socio').controller('Socio.CuentaPersonal.EditarCuentaPersonal.TitularesController',
-    function ($scope, $state, cuentaPersonal, CuentaBancariaService) {
+    function ($scope, $state, toastr, cuentaPersonal, CuentaBancariaService, PersonaNaturalService) {
 
         $scope.view = {
-            cuentaPersonal: cuentaPersonal
+            cuentaPersonal: cuentaPersonal,
+            titular: {}
         };
 
         $scope.view.load = {
-            titulares: []
+            titulares: [],
+            persona: undefined
         };
+
+        $scope.combo = {
+            tipoDocumento: undefined
+        };
+        $scope.combo.selected = {
+            tipoDocumento: undefined
+        };
+
+        $scope.loadCombo = function(){
+            PersonaNaturalService.getTipoDocumentos().then(function (response) {
+                $scope.combo.tipoDocumento = response;
+            });
+        };
+        $scope.loadCombo();
 
         $scope.loadTitulares = function () {
             CuentaBancariaService.getTitulares($scope.view.cuentaPersonal.id).then(function (response) {
@@ -19,5 +35,18 @@ angular.module('socio').controller('Socio.CuentaPersonal.EditarCuentaPersonal.Ti
         };
         $scope.loadTitulares();
 
+        $scope.check = function ($event) {
+            if (!angular.isUndefined($event)) {
+                $event.preventDefault();
+            }
+            PersonaNaturalService.findByTipoNumeroDocumento($scope.combo.selected.tipoDocumento.id, $scope.view.titular.numeroDocumento).then(function (response) {
+                $scope.view.load.persona = response;
+                if ($scope.view.load.persona) {
+                    toastr.info('Persona encontrada');
+                } else {
+                    toastr.warning('Persona no encontrada');
+                }
+            });
+        };
 
     });

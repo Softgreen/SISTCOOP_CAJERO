39798d2a582e30a904c5336e37e90662a8ee679b
Carlos <carlosthe19916@gmail.com>
2015-11-06 18:57:37 -0500

    crear pendiente

---
+++ modules/cooperativa/client/controllers/administracion/pendiente/crear.client.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* jshint -W098 */
+angular.module('cooperativa').controller('Cooperativa.Administracion.Pendiente.CrearController',
+    function ($scope, $state, CAJA, CajaService) {
+
+        $scope.view = {};
+
+
+    }
+);
